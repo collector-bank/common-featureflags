2058dd1f5ce31b42521c5ae5b09f0627f79c3d45
Adam Lith <adam.lith@gmail.com>
2018-02-13 12:20:02 +0100

    Adding a static property to hold a provider. (#1)

---
+++ src/Collector.Common.FeatureFlags/FeatureFlagProvider.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FeatureFlagProvider.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Collector.Common.FeatureFlags
+{
+    public static class FeatureFlagProvider
+    {
+        public static IFeatureFlagProvider Instance { get; set; }
+    }
+}
